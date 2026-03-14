a270d53e6cb5948fc7bf580b879378607c0fbc8c
saran <sn4551669@gmail.com>
2026-03-14 13:35:21 +0530

    Create inputreadwrite.java

---
+++ inputreadwrite.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+            sc.nextLine();
+        String b=sc.nextLine();
+
+        System.out.println(a*2);
+        System.out.println(b);
+    }
+}
